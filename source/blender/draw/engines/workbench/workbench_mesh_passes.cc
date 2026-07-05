1a4429e5fb049d4d330e98ede599991c50d6d777
dexapex377-coder <dexapex377@gmail.com>
2026-07-05 02:06:52 +0000

    DIAG: Add CLOG_INFO + __android_log_print entry diagnostic for Workbench draw

--- source/blender/draw/engines/workbench/workbench_mesh_passes.cc
+++ source/blender/draw/engines/workbench/workbench_mesh_passes.cc
@@ -1,5 +1,10 @@
 /* SPDX-License-Identifier: GPL-2.0-or-later */
 
+#include <android/log.h>
+#include <CLG_log.h>
+
+static CLG_LogRef LOG = {"workbench.mesh_passes"};
+
 #include "workbench_private.hh"
 
 namespace blender::workbench {
@@ -156,6 +161,9 @@ void OpaquePass::draw(Manager &manager,
   if (is_empty()) {
     return;
   }
+  CLOG_INFO(&LOG, 1, "Workbench: OpaquePass::draw entered");
+  __android_log_print(ANDROID_LOG_INFO, "Blender.Workbench",
+      "Workbench: OpaquePass::draw entered");
   gbuffer_material_tx.acquire(
       resolution, GPU_RGBA16F, GPU_TEXTURE_USAGE_SHADER_READ | GPU_TEXTURE_USAGE_ATTACHMENT);
   gbuffer_normal_tx.acquire(
