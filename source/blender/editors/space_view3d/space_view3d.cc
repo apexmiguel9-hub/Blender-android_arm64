ce851bec8099875e0b7d1c07d83a1e177e2290b1
root <root@localhost.localdomain>
2026-07-13 19:37:43 +0000

    Revert "view3d: add 3D Cursor gizmo (translate arrows at cursor position)"
    This reverts commit 6a89f972af2266638f4fc8f02d6b1782cc45c646.

--- source/blender/editors/space_view3d/space_view3d.cc
+++ source/blender/editors/space_view3d/space_view3d.cc
@@ -1050,7 +1050,6 @@ static void view3d_widgets()
   WM_gizmogrouptype_append(VIEW3D_GGT_ruler);
   WM_gizmotype_append(VIEW3D_GT_ruler_item);
 
-  WM_gizmogrouptype_append(VIEW3D_GGT_cursor3d);
   WM_gizmogrouptype_append(VIEW3D_GGT_placement);
 
   WM_gizmogrouptype_append_and_link(gzmap_type, VIEW3D_GGT_navigate);
