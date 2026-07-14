a0129f360ba72e36711e7431fc8db3f9bfc5b707
root <root@localhost.localdomain>
2026-07-14 02:43:13 +0000

    cursor_gizmo: back to working circle, put widget= back

--- scripts/startup/bl_ui/cursor_gizmo.py
+++ scripts/startup/bl_ui/cursor_gizmo.py
@@ -1,7 +1,15 @@
 # SPDX-License-Identifier: GPL-2.0-or-later
 
 import bpy
-from bpy.types import GizmoGroup
+from bpy.types import Gizmo, GizmoGroup
+from mathutils import Matrix
+
+
+class VIEW3D_GT_cursor_widget(bpy.types.Gizmo):
+    bl_idname = "VIEW3D_GT_cursor_widget"
+
+    def draw(self, context):
+        self.draw_preset_circle(self.matrix_basis, axis='POS_Z')
 
 
 class VIEW3D_GGT_cursor_indicator(GizmoGroup):
@@ -24,20 +32,19 @@ class VIEW3D_GGT_cursor_indicator(GizmoGroup):
         return tool.idname == "builtin.cursor"
 
     def setup(self, context):
-        try:
-            gz = self.gizmos.new("GIZMO_GT_move_3d")
-            gz.target_set_prop("offset", context.scene.cursor, "location")
+        gz = self.gizmos.new(VIEW3D_GT_cursor_widget.bl_idname)
+        gz.color = 0.8, 0.8, 0.2
+        gz.alpha = 0.6
+        gz.color_highlight = 1.0, 1.0, 0.5
+        gz.alpha_highlight = 1.0
+        gz.scale_basis = 0.3
+        self._indicator = gz
 
-            gz.color = 0.8, 0.8, 0.8
-            gz.alpha = 0.5
-            gz.color_highlight = 1.0, 1.0, 1.0
-            gz.alpha_highlight = 1.0
-            gz.scale_basis = 0.5
+    def draw_prepare(self, context):
+        self._indicator.matrix_basis = Matrix.Translation(context.scene.cursor.location)
 
-            self._gizmo = gz
-        except:
-            import traceback
-            traceback.print_exc()
 
-    def refresh(self, context):
-        pass
+classes = (
+    VIEW3D_GT_cursor_widget,
+    VIEW3D_GGT_cursor_indicator,
+)
