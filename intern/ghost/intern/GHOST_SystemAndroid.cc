f97bac05fa2fd6c05155c469ab14f7b8b86d6d3d
root <root@localhost.localdomain>
2026-07-29 12:29:16 +0000

    fix: move 10010 handler outside if(num==1) so num>=3 works

--- intern/ghost/intern/GHOST_SystemAndroid.cc
+++ intern/ghost/intern/GHOST_SystemAndroid.cc
@@ -1976,6 +1976,18 @@ void GHOST_SystemAndroid::setViewportBounds(int32_t xmin, int32_t ymin, int32_t 
 void GHOST_SystemAndroid::setValue(int values[], int num) {
     GHOST_WindowAndroid *window = (GHOST_WindowAndroid *) getWindowManager()->getActiveWindow();
     char utf8_char[6] = {0};
+    // Multi-value codes (num > 1) must be handled outside the single-value block.
+    if (num >= 3 && values[0] == 10010) {
+        /* Set cursor position (virtual cursor). */
+        GHOST_WindowAndroid *win = (GHOST_WindowAndroid *) getWindowManager()->getActiveWindow();
+        if (win) {
+            GHOST_TabletData td;
+            pushEvent(new GHOST_EventCursor(getMilliSeconds(), GHOST_kEventCursorMove, win, values[1], values[2], td));
+        }
+        m_x = values[1];
+        m_y = values[2];
+        return;
+    }
     if (num == 1) {
         OBLButtonID oblButtonId = (OBLButtonID) values[0];
         int oblButtonNum = sizeof(oBLButtonIDGhostKeys) / sizeof(oBLButtonIDGhostKeys[0]);
@@ -2062,15 +2074,6 @@ void GHOST_SystemAndroid::setValue(int values[], int num) {
             /* Toggle scroll mode. */
             m_scrollMode = !m_scrollMode;
 
-        } else if (values[0] == 10010 && num >= 3) {
-            /* Set cursor position (virtual cursor). */
-            GHOST_WindowAndroid *win = (GHOST_WindowAndroid *) getWindowManager()->getActiveWindow();
-            if (win) {
-                GHOST_TabletData td;
-                pushEvent(new GHOST_EventCursor(getMilliSeconds(), GHOST_kEventCursorMove, win, values[1], values[2], td));
-            }
-            m_x = values[1];
-            m_y = values[2];
         }
     }
 }
